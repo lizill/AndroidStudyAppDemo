7fd94464681ab3b3b0b2f536392ba1fe05041a39
DONG <lizill@naver.com>
2021-03-26 22:41:28 +0900

    First Screen

---
+++ app/src/main/java/com/example/studyapp/FirstActivity.java
@@ -0,0 +1,36 @@
+package com.example.studyapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private Button signInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_first);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        signInButton = (Button) findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, RegisterActivity.class);
+                FirstActivity.this.startActivity(intent);
+//                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/studyapp/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.studyapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_register);
+
+        ActionBar actionBar = getSupportActionBar();
+    }
+}
