7d918183ec572a7d1dd9da3755a5b76e62feff08
deliyami <dkdidudtn@gmail.com>
2021-05-17 11:07:20 +0900

    commit

--- app/src/main/java/com/example/studyapp/ui/plan/PlanViewModel.java
+++ app/src/main/java/com/example/studyapp/ui/plan/PlanViewModel.java
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel;
 public class PlanViewModel extends ViewModel {
     private MutableLiveData<String> mText;
     public PlanViewModel() {
-        // 아무것도 없으면 기본적으로 띄울 Text
+        // PlanFragment에서 불러온 Fragment가 아무것도 없을 경우 기본적으로 띄울 Text
         mText = new MutableLiveData<>();
         mText.setValue("This is plan fragment");
     }
