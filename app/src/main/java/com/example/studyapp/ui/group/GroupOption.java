a5b4082c500cdebf7d2631717f53862f9449bd51
jja33705 <jja33705@gmail.com>
2021-05-18 15:00:37 +0900

    test

---
+++ app/src/main/java/com/example/studyapp/ui/group/GroupOption.java
@@ -0,0 +1,14 @@
+package com.example.studyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_option);
+    }
+}
